f3f220c3fe285beaf1f2ada71f7cd2a63faf64a1
Albert Wong <albertium.land@gmail.com>
2015-04-27 07:29:36 -0400

    add EDA, GLM code

--- titanic/helpers.py
+++ titanic/helpers.py
@@ -1,4 +1,7 @@
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, \
+		PolynomialFeatures
+import numpy as np
 import pandas as pd
 
 class NAFiller:
@@ -7,9 +10,9 @@ class NAFiller:
         self.method = method
     
     def fit(self, X):
-        if self.method = 'mean':
+        if self.method == 'mean':
             self.filling = X[self.cols].mean(skipna=True)
-        elif self.method = 'median':
+        elif self.method == 'median':
             self.filling = X[self.cols].median(skipna=True)
         else:
             raise ValueError('method only accpets mean and median')
@@ -17,9 +20,108 @@ class NAFiller:
     def transform(self, X):
         return X.fillna(self.filling)
 
-class BayesClassifier(BaseEstimator):
-	def __init__(self):
-		pass
 
-	def fit(self, X, y):
+class MultiLabelEncoder(BaseEstimator, TransformerMixin):
+	def __init__(self, cols=None):
+		if not cols:
+			raise ValueError('columns are not specified')
+
+		self.cols = cols
+		self.le = dict()
+		for col in cols:
+			self.le[col] = LabelEncoder()
+
+	def fit(self, X, y=None):
+		for col, le in self.le.iteritems():
+			le.fit(X[:, col])
+			
+	def transform(self, X):
+		X = X.copy()
+		for col, le in self.le.iteritems():
+			X[:, col] = le.transform(X[:, col])
+
+		return np.array(X, dtype='float32')
+
+	def fit_transform(self, X, y=None):
+		X = X.copy()
+		for col, le in self.le.iteritems():
+			X[:, col] = le.fit_transform(X[:, col])
+
+		return np.array(X, dtype='float32')
+
+
+class OneHotEncoderMinusOne(BaseEstimator, TransformerMixin):
+	def __init__(self, cols=None):
+		if not cols:
+			raise ValueError('columns are not specified')
+
+		self.cols = cols
+		self.oh = dict()
+		for col in cols:
+			self.oh[col] = OneHotEncoder(sparse=False) 
+
+	def fit(self, X, y=None):
+		for col, oh in self.oh.iteritems():
+			oh.fit(X[:, col:col+1])
+
+	def transform(self, X):
+		tmp = np.hsplit(X, X.shape[1])
+		for col, oh in self.oh.iteritems():
+			tmp[col] = oh.transform(X[:, col:col+1])[:, 1:]
+
+		return np.hstack(tmp)
+
+	def fit_transform(self, X, y=None):
+		tmp = np.hsplit(X, X.shape[1])
+		for col, oh in self.oh.iteritems():
+			tmp[col] = oh.fit_transform(X[:, col:col+1])[:, 1:]
+
+		return np.hstack(tmp)
+
+
+class GenericPolyFeatures(BaseEstimator, TransformerMixin):
+	def __init__(self, non_dummy_features, degree=2):
+		self.conts = non_dummy_features
 		
+		self.poly_cross = \
+			PolynomialFeatures(degree=degree, \
+								interaction_only=True, \
+								include_bias=False)
+		self.poly_cont = \
+			PolynomialFeatures(degree=degree, include_bias=False)
+
+	def fit(self, X, y=None):
+		# standardize continuous feature index
+		self.conts = np.arange(X.shape[1])[self.conts]
+		# intialize for cross terms
+		self.poly_cross.fit(X)
+		self.poly_cont.fit(X[:, :1])
+		return self
+
+	def transform(self, X):
+		# transform for interction only and exclude duplicate zeros
+		cross = self.poly_cross.transform(X)
+		cross = cross[:, -np.all(cross==0, axis=0)]
+
+		# transform for continuous features
+		cont = np.hstack([self.poly_cont.transform(X[:, idx:idx+1])[:, 1:] \
+					for idx in self.conts])
+
+		# combine interaction, dummies and continuous features
+		return np.hstack([np.ones((X.shape[0], 1)), cross, cont])
+
+	def fit_transform(self, X, y=None):
+		# standardize continuous feature index
+		self.conts = np.arange(X.shape[1])[self.conts]
+
+		# transform for interction only and exclude duplicate zeros
+		cross = self.poly_cross.fit_transform(X)
+		cross = cross[:, -np.all(cross==0, axis=0)]
+
+		# transform for continuous features
+		cont = np.hstack( \
+				[self.poly_cont.fit_transform(X[:, idx:idx+1])[:, 1:] \
+					for idx in self.conts])
+
+		# combine interaction, dummies and continuous features
+		return np.hstack([np.ones((X.shape[0], 1)), cross, cont])
